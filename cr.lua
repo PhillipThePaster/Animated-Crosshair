127530e32ce5b907bdea68f06c4569e13da865e7
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-10-04 08:34:14 -0400

    Update cr.lua

--- cr.lua
+++ cr.lua
@@ -3,19 +3,19 @@ local Camera = game:GetService("Workspace").CurrentCamera
 local RunService = game:GetService("RunService")
 
 local settings = {
-    Visible = true,
-    size = 10,
-    spinSpeed = 2,
-    SpinAnimation = true,
-    pos = Vector2.new(0, 0)
-	
+    Visible = true;
+    size = 10;
+    spinSpeed = 2;
+    SpinAnimation = true;
+    pos = Vector2.new(0, 0);
+    Thickness = 1;
 }
 
 local lineSets = {
-    {count = settings.size, thickness = 1, direction = 0, visible = settings.Visible},   -- Bottom
-    {count = settings.size, thickness = 1, direction = 90, visible = settings.Visible},  -- Top
-    {count = settings.size, thickness = 1, direction = 180, visible = settings.Visible}, -- Right
-    {count = settings.size, thickness = 1, direction = 270, visible = settings.Visible}  -- Left
+    {count = settings.size, settings.Thickness, direction = 0, visible = settings.Visible},   -- Bottom
+    {count = settings.size, settings.Thickness, direction = 90, visible = settings.Visible},  -- Top
+    {count = settings.size, settings.Thickness, direction = 180, visible = settings.Visible}, -- Right
+    {count = settings.size, settings.Thickness, direction = 270, visible = settings.Visible}  -- Left
 }
 local drawings = {}
 
